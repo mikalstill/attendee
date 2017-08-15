3d9ffea1adcb8105de413396cac3bf729592993f
Michael Still <mikal@stillhq.com>
2017-08-15 14:15:57 +1000

    Working with one reader and blocking reads.

--- reader.py
+++ reader.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python
 
-import usb.core
-import usb.util
 
-# Find our device
+from keyboard_alike import reader
 
-def filt(dev):
-    # NOTE(mikal): this is written like this because I am sure there will be
-    # other devices we want to match later and I am unsure that this vendor /
-    # product combination is unique to this device.
-    if dev.idVendor == 0xffff and dev.idProduct == 0x0035:
-        return True
-    return False
 
-for dev in usb.core.find(find_all=True, custom_match=filt):
-    print dev
-    dev.set_configuration()
+if __name__ == "__main__":
+    reader = reader.Reader(0xffff, 0x0035, 84, 16, should_reset=False,
+                           debug=False)
+    reader.initialize()
+    print(reader.read().strip())
+    reader.disconnect()
+
